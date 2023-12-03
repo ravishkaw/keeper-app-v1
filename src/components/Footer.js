ece63d5ab7bb76fe820f348f3705cc524ea40481
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-12-03 11:48:29 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>©️ {year} Ravishka Wijerathne</p>
+    </footer>
+  );
+}
+
+export default Footer;
