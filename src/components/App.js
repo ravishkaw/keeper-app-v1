13a25a99825bceeea6290a312cb22b156a2a4b20
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-12-04 13:23:13 +0530

    updated to work with dynamic notes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,21 @@ import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map((noteContent) => {
+        return (
+          <Note
+            key={noteContent.key}
+            title={noteContent.title}
+            content={noteContent.content}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
